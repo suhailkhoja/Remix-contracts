967ef74798767f85210ab6ec706d79f5460a0ed2
suhailkhoja <k_suhails@hotmail.com>
2023-07-11 06:04:48 -0400

    Contract14

---
+++ contracts/Bank-Lecture10.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.6;
+
+contract Bank {
+    mapping (address => uint) public balance;
+    address owner;
+
+event balanceAdded(uint amount, address depositedTo);
+
+    modifier onlyOwner {
+        require (owner == msg.sender, "You are not the owner");
+        _;
+    }
+
+    constructor () {
+        owner = msg.sender;
+    }
+
+    function addBalance(uint _toAdd) public returns (uint) {
+        balance[msg.sender] += _toAdd;
+        emit balanceAdded (_toAdd, msg.sender);
+        return balance[msg.sender];
+    } 
+
+    function getBalance () public view returns (uint) {
+        return balance[msg.sender];
+    }
+
+    function transfer(address recipient, uint amount) public onlyOwner {
+        
+        require (msg.sender != recipient, "Sender and recipient couldn't be same");
+        require (balance [msg.sender] >= amount, "You don't have sufficient funds to send.");
+        uint previousSenderBalance = balance[msg.sender];
+        balance [msg.sender] -= amount;
+        balance [recipient] += amount;
+
+        assert (balance[msg.sender] == previousSenderBalance - amount);
+
+    }
+}
