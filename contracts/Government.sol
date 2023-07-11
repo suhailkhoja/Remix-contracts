4db65fb4ce2c08cff6194c2ead55aa45ea2f94c0
suhailkhoja <k_suhails@hotmail.com>
2023-07-11 06:09:42 -0400

    Contract23

---
+++ contracts/Government.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.7.5;
+
+contract Government {
+
+    struct Transaction {
+        address from;
+        address to;
+        uint amount;
+        uint txId;
+    }
+
+    Transaction[] transactionLog;
+
+    function addTransaction (address _from, address _to, uint _amount) external {
+        Transaction memory _transaction = Transaction(_from, _to, _amount, transactionLog.length);
+        transactionLog.push(_transaction);
+    }
+
+    function getTransaction (uint _index) public view returns (address, address, uint) {
+        return (transactionLog[_index].from, transactionLog[_index].to, transactionLog[_index].amount);
+    }
+}
