0a99fb8df9650cdb41f61053d7a0284cd7e33c3c
suhailkhoja <k_suhails@hotmail.com>
2023-07-11 06:02:23 -0400

    Contract12

---
+++ contracts/Lecture9.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.7.5;
+
+contract DataLocation {
+
+    function getString (string memory text) public pure returns(string memory) {
+        uint secondNumber = 5;
+        return text;
+    }
+}
